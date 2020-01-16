612d988ecc6c58ab673d3962fb093288596f24ed
Connor Wong <connorwong93@gmail.com>
2020-01-15 17:53:02 -0800

    commit before refactor

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$YpykOHga9E2VNO.SyMgi2OnKdpFH2C29fGuATgWwUwKHT0fl5noKy',
 }
